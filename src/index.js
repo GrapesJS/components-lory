acd5439f54ec5269dcf98ff0be42f384645fd92b
Artur Arseniev <artur.catch@hotmail.it>
2018-01-07 14:42:38 +0100

    Add `prevEl` and `nextEl` options

--- src/index.js
+++ src/index.js
@@ -23,6 +23,16 @@ export default grapesjs.plugins.add('grapesjs-lory-slider', (editor, opts = {}) 
     // Object to extend the default next nav properties
     nextProps: {},
 
+    // Previous nav element string, as for example, an HTML string
+    prevEl: `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5">
+        <g><path fill="#2E435A" d="M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"/></g>
+      </svg>`,
+
+    // Next nav element string, as for example, an HTML string
+    nextEl: `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5">
+        <g><path fill="#2E435A" d="M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z"/></g>
+      </svg>`,
+
     // Script to load dynamically in case no lory instance found
     script: 'https://cdnjs.cloudflare.com/ajax/libs/lory.js/2.3.4/lory.min.js',
 
@@ -30,10 +40,6 @@ export default grapesjs.plugins.add('grapesjs-lory-slider', (editor, opts = {}) 
 
     // Default slides
     slidesEl: '',
-
-    prevEl: '',
-
-    nextEl: '',
   },  ...opts };
 
   // Add components
